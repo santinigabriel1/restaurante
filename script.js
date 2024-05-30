ebba7901d8945fc5315e10d4086ab07842f099a2
Gabriel Santini <santinigabriel.dev@gmail.com>
2024-05-30 12:01:39 -0300

    atribuindo funções ao carrinho

--- script.js
+++ script.js
@@ -58,3 +58,158 @@ $(document).ready(function() {
         distance: '20%'
     });
 });
+
+// cart functions
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("btn_cart");
+const cartModal = document.getElementById("modal");
+const cartItemsContainer = document.getElementById("cart_items");
+const cartTotal = document.getElementById("cart_total");
+const checkoutBtn = document.getElementById("checkout_btn");
+const closeModalBtn = document.getElementById("close_modal_btn");
+const cartCounter = document.getElementById("cart_count");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address_warn");
+
+let cart = [];
+
+cartBtn.addEventListener("click", function() {
+    updateCarModal();
+    cartModal.style.display = "flex";
+});
+
+cartModal.addEventListener("click", function() {
+    if(event.target === cartModal){
+        cartModal.style.display = "none";
+    }
+});
+
+closeModalBtn.addEventListener("click", function() {
+    cartModal.style.display = "none";
+});
+
+menu.addEventListener("click", function(event) {
+    let  parentButton = event.target.closest(".btn-cart");
+    
+    if(parentButton) {
+        const name = parentButton.getAttribute("data-name");
+        const price = parseFloat(parentButton.getAttribute("data-price"));
+
+        addToCart(name, price)
+    }
+});
+
+function addToCart(name, price) {
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem) {
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        });
+    }
+
+    updateCarModal()
+
+}
+
+function updateCarModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col");
+
+        cartItemElement.innerHTML = `
+            <div class="flex items-center justify-between">
+
+                <div>
+                    <p class="font-bold">${item.name}</p>
+                    <p>Qtd: ${item.quantity}</p>
+                    <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
+                </div>
+
+                <button class="remove-btn" data-name="${item.name}">
+                        Remover
+                </button>
+            </div>
+        `
+
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+}
+
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-btn")){
+        const name = event.target.getAttribute("data-name");
+
+        removeItemCart(name);
+    }
+});
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+        
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCarModal();
+            return;
+        }
+
+        cart.splice(index, 1);
+        updateCarModal();
+    }
+}
+
+addressInput.addEventListener("input", function(){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+});
+
+checkoutBtn.addEventListener("click", function(){
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+});
+
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+}
+
+const spanItem = document.getElementById("date_span")
+const isOpen = checkRestaurantOpen();
+
+if (isOpen) {
+    spanItem.style.backgroundColor = "#34D399";
+    spanItem.classList.remove("bg-red");
+    spanItem.classList.add("bg-green");
+} else {
+    spanItem.style.backgroundColor = "#EF4444"; 
+    spanItem.classList.remove("bg-green");
+    spanItem.classList.add("bg-red");
+}
